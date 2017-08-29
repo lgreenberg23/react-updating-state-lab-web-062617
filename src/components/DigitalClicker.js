f333f45c3973e075cb6801d096ebeb60a3408cfc
github username <github email address>
2017-08-29 16:33:12 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,34 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component{
+
+	constructor(props){
+		super(props)
+		this.state = {timesClicked: 0}
+	}
+
+
+	increment = (event) => this.setState({timesClicked: this.state.timesClicked+=1})
+
+	render(){
+		return <button onClick={this.increment}>{this.state.timesClicked}</button>
+	}
+}
+
+
+
+export default DigitalClicker
+
+
+
+
+
+
+
+// In the components/DigitalClicker.js file, create a DigitalClicker React component.
+// This component has an initial state property called timesClicked, which is initially 
+// defined as 0.
+// The component renders out a button with a label that shows the timesClicked value. 
+// This means that, at the start, your button should just say 0.
+// Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,71 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react'
+
+class YouTubeDebugger extends React.Component{
+
+	constructor(props){
+		super(props)
+
+		this.state = {
+			errors: [], 
+			user: null, 
+			settings: { 
+				bitrate: 8, 
+				video: { 
+					resolution: '1080p' }
+			} 
+		}
+	}
+
+	// const newRes = { resolution: '720p' }
+	// const newBitrate = { bitrate: 12 }
+
+	changeBitrate = (event) => {
+		this.setState({
+		settings: {
+			...this.state.settings, bitrate: 12
+			},
+		});
+	}
+		
+
+	changeRes = (event) => {
+		this.setState({
+		settings: {
+			...this.state.settings, 
+			video: {
+		 	...this.state.settings.video, resolution: '720p' },
+			},
+		})
+	}
+
+
+	render(){
+		return (
+			<div>
+				<button className='bitrate' onClick={this.changeBitrate}>Change bits</button>
+				<button className='resolution' onClick={this.changeRes}>Change res</button>
+			</div>
+		)
+	}
+}
+
+
+
+export default YouTubeDebugger
+
+// this.setState({
+//   addressInfo: {
+//     ...this.state.addressInfo,
+//     city: 'New York City',
+//   },
+// });
+
+// This component has several state properties. The initial state shape looks 
+// like this: 
+// js { errors: [], user: null, settings: { bitrate: 8, video: { resolution: '1080p' } } }
+// Create a button with the class 'bitrate'. Clicking this button changes the 
+// settings.bitrate state property to 12.
+// Create a button with the class 'resolution'. Clicking this button changes the 
+// settings.video.resolution state property to '720p'.
